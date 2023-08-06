1800b8e09b06ee8edadff351e72b6fe09808bf9e
Leo <leonid20000@gmail.com>
2023-08-06 21:09:10 +0800

    updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="odin-slides",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         'python-pptx',
@@ -17,4 +17,9 @@ setup(
         ],
     },
     test_suite="tests",
+
+    description="An advanced Python tool that empowers you to effortlessly draft impressive PowerPoint presentations. Leveraging the capabilities of Language Models (LLM), odin-slides enables content summarization, slide generation, and seamless presentation creation or updating based on user input",
+    url="https://github.com/leonid20000/odin-slides",
+    author="Dr. Leonit Zeynalvand",
+    license="MIT",    
 )
