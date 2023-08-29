e61c1a0a85d95c7d024bd4c7502573faf2fa0fff
Leo <leonid20000@gmail.com>
2023-08-29 19:24:44 +0800

    updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="odin-slides",
-    version="0.3",
+    version="0.5",
     packages=find_packages(),
     install_requires=[
         'python-pptx',
